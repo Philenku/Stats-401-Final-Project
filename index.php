005eb1dad6c33fe9e70d5e51f19c03ad2d70e24e
eliyanovva <teodorapetkovax@gmail.com>
2022-10-19 19:53:10 -0400

    added explanation for 3 and 5

---
+++ index.php
@@ -0,0 +1,33 @@
+<head>
+    <title>
+        STATS 401 Final Project Group 10
+    </title>
+    <style>
+        body {
+            text-align:center;
+        }
+        h1 {
+            color:green;
+        }
+    </style>
+</head>
+  
+<body>
+   <?php echo shell_exec("python visual3.py ");
+    echo shell_exec("python visual5.py ");?>
+    
+    <h1>STATS 401 Final Project</h1>
+      
+    <h3>
+        Click on the following links to see our visuals.
+    </h3>
+
+    
+    <a href="http://127.0.0.1:8988/">
+      See Visual 3
+   </a>
+    <a href="http://127.0.0.1:8989/">
+        See Visual 5
+    </a>
+</body>
+  
